454cf38f0f9e9ce836cae294de248c7413aa1195
kareemadesola <kareemadesola1999@gmail.com>
2021-09-10 16:46:20 +0100

    delete currency application

--- app/src/main/java/com/example/currencyconversion/data/CurrencyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currencyconversion.data
-
-import android.app.Application
-
-class CurrencyApplication: Application() {
-    // Using by lazy so the database is only created when needed
-    // rather than when the application starts
-    val database: CurrencyRoomDatabase by lazy { CurrencyRoomDatabase.getDatabase(this) }
-}
