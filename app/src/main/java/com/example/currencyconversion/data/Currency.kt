689511a02d2661db02e2846ac5e3552ded408cbd
kareemadesola <kareemadesola1999@gmail.com>
2021-09-10 10:31:55 +0100

    create data class for Currency database table

---
+++ app/src/main/java/com/example/currencyconversion/data/Currency.kt
@@ -0,0 +1,18 @@
+package com.example.currencyconversion.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity data class represents a single row in a database
+ */
+@Entity
+data class Currency(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "code")
+    val currencyCode: String,
+    @ColumnInfo(name = "us_dollar_index")
+    val USDollarIndex: Double
+)
