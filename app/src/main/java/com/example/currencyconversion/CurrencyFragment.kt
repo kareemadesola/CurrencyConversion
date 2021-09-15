68c46719fa97eeefd96aff7bcbe148ec8494fb39
kareemadesola <kareemadesola1999@gmail.com>
2021-09-15 09:36:02 +0100

    move recycler view code to onCreate method

--- app/src/main/java/com/example/currencyconversion/CurrencyFragment.kt
+++ app/src/main/java/com/example/currencyconversion/CurrencyFragment.kt
@@ -1,7 +1,6 @@
 package com.example.currencyconversion
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +8,9 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.currencyconversion.data.CurrencyView
 import com.example.currencyconversion.databinding.FragmentCurrencyBinding
 
 
@@ -21,7 +21,7 @@ import com.example.currencyconversion.databinding.FragmentCurrencyBinding
  */
 class CurrencyFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
-    private val viewModel: CurrencyViewModel by activityViewModels()
+    private val viewModel: CurrencyViewModel by viewModels()
 
     // Binding object instance corresponding to the fragment_currency.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
@@ -37,42 +37,33 @@ class CurrencyFragment : Fragment(), AdapterView.OnItemSelectedListener {
         // Inflate the layout for this fragment
         _binding = FragmentCurrencyBinding.inflate(inflater, container, false)
         // Create an ArrayAdapter using the string array and a default spinner layout
-        /*context?.let {
-            viewModel.transformedAPIData.value?.map { currencyList ->
-                currencyList.currencyCode
-            }.let { currencyCodeList ->
-                ArrayAdapter(
-                    it,
-                    android.R.layout.simple_spinner_item,
-                    currencyCodeList!!
-                ).also { adapter ->
-                    // Specify the layout to use when the list of choices appears
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                    // Apply the adapter to the spinner
-                    binding.baseCurrencySpinner.adapter = adapter
-                    binding.baseCurrencySpinner.onItemSelectedListener = this
-                }
-            }
-        }*/
 //        ArrayAdapter(context, android.R.layout.simple_spinner_item, listOf(1,2,3))
 
-        viewModel.transformedAPIData.observe(viewLifecycleOwner) { currencyViewList ->
-            Log.i("transformedApiData", viewModel.rawAPIData.value.toString())
-            Log.i("transformedApiData", currencyViewList.toString())
+        viewModel.spinnerData.observe(viewLifecycleOwner) { currencyCodeList ->
             context?.let {
                 ArrayAdapter(
                     it, android.R.layout.simple_spinner_item,
-                    currencyViewList.map { currencyView -> currencyView.currencyCode })
+                    currencyCodeList
+                ).also { adapter ->
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    binding.baseCurrencySpinner.adapter = adapter
+                }
             }
         }
+        val adapter = CurrencyListAdapter()
+        binding.recyclerView.adapter = adapter
+
+        viewModel.transformedAPIData.observe(viewLifecycleOwner){ currencyViewList ->
+            currencyViewList.let {
+                adapter.submitList(it)
+            }
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = CurrencyListAdapter()
-        binding.recyclerView.layoutManager = LinearLayoutManager(context)
-        binding.recyclerView.adapter = adapter
 
         binding.amountToConvert.doAfterTextChanged { text ->
             viewModel.setAmount(text?.toString())
