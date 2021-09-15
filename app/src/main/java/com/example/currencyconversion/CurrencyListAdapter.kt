e259e54eb06e71dfe860b0099e78ea4dcf58f086
kareemadesola <kareemadesola1999@gmail.com>
2021-09-15 15:24:21 +0100

    implement DiffUtil getChangePayload

--- app/src/main/java/com/example/currencyconversion/CurrencyListAdapter.kt
+++ app/src/main/java/com/example/currencyconversion/CurrencyListAdapter.kt
@@ -13,11 +13,18 @@ class CurrencyListAdapter :
 
     companion object DiffCallback : DiffUtil.ItemCallback<CurrencyView>() {
         override fun areItemsTheSame(oldItem: CurrencyView, newItem: CurrencyView): Boolean {
-            return oldItem === newItem
+            return oldItem.currencyCode == newItem.currencyCode
         }
 
         override fun areContentsTheSame(oldItem: CurrencyView, newItem: CurrencyView): Boolean {
-            return oldItem.currencyCode == newItem.currencyCode
+            return oldItem == newItem
+        }
+
+        override fun getChangePayload(oldItem: CurrencyView, newItem: CurrencyView): Any? {
+            return when {
+                oldItem.convertedAmount != newItem.convertedAmount -> true
+                else -> null
+            }
         }
 
     }
@@ -30,6 +37,9 @@ class CurrencyListAdapter :
             binding.convertedAmount.text = currencyView.convertedAmount
             binding.currencyFullName.text = currencyView.currencyFullName
         }
+        fun bindConvertedAmount(currencyView: CurrencyView) {
+            binding.convertedAmount.text = currencyView.convertedAmount
+        }
     }
 
     override fun onCreateViewHolder(
@@ -41,5 +51,24 @@ class CurrencyListAdapter :
 
     override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
         holder.bind(getItem(position))
+    }
+
+    override fun onBindViewHolder(
+        holder: CurrencyViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        when {
+            payloads.isEmpty() -> {
+                super.onBindViewHolder(holder, position, payloads)
+            }
+            else -> {
+                when {
+                    payloads[0] == true -> {
+                        holder.bindConvertedAmount(getItem(position))
+                    }
+                }
+            }
+        }
     }
 }
