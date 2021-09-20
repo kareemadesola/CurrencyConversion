f1a31ac9b19253ae6b99a5aebdb22ac92c7c2a55
kareemadesola <kareemadesola1999@gmail.com>
2021-09-20 15:45:08 +0100

    work in progress

--- app/src/main/java/com/example/currencyconversion/CurrencyViewModel.kt
+++ app/src/main/java/com/example/currencyconversion/CurrencyViewModel.kt
@@ -6,9 +6,10 @@ import com.example.currencyconversion.data.CurrencyView
 import com.example.currencyconversion.network.CurrencyApi
 import com.example.currencyconversion.network.CurrencyResponse
 import kotlinx.coroutines.launch
+import java.util.*
 
 class CurrencyViewModel : ViewModel() {
-    fun setAmount(amount: String?){
+    fun setAmount(amount: String?) {
         _amountToConvert.value = amount?.toDoubleOrNull() ?: 0.0
     }
 
@@ -17,25 +18,40 @@ class CurrencyViewModel : ViewModel() {
 
 
     private val _rawAPIData: MutableLiveData<CurrencyResponse> by lazy { MutableLiveData<CurrencyResponse>() }
-    val rawAPIData: LiveData<CurrencyResponse> = _rawAPIData
+    private val rawAPIData: LiveData<CurrencyResponse> = _rawAPIData
 
-    val transformedAPIData: LiveData<List<CurrencyView>> =
-        Transformations.switchMap(rawAPIData) { getTransformedData(it) }
+    val transformedAPIData: LiveData<List<CurrencyView>> = Transformations.switchMap(rawAPIData) { getTransformedData(it) }
 
     private fun getTransformedData(response: CurrencyResponse): LiveData<List<CurrencyView>> {
-        return amountToConvert.map { amount->
-            response.quotes.toList().map { it ->
+        return amountToConvert.map { amount ->
+            response.quotes.toList().map { currencyPair ->
                 CurrencyView(
-                    it.first.takeLast(3),
-                    (it.second * amount).let { String.format("%.2f", it) },
-                    java.util.Currency.getInstance(it.first.takeLast(3)).displayName
+                    currencyPair.first.takeLast(3),
+                    (amount.times(currencyPair.second)).let { String.format("%.2f", it) },
+                    Currency.getInstance(currencyPair.first.takeLast(3)).displayName
                 )
             }
         }
-
     }
+
+
     val spinnerData: LiveData<List<String>> = Transformations.map(rawAPIData){ currencyResponse ->
-        currencyResponse.quotes.toList().map { it.first.takeLast(3) }
+        currencyResponse.quotes.toList().map {
+            it.first.takeLast(3)
+        }
+    }
+
+    private val _baseCurrencyRate: MutableLiveData<Double> by lazy { MutableLiveData<Double>(1.0) }
+    val baseCurrencyRate: LiveData<Double> = _baseCurrencyRate
+
+    fun setBaseCurrencyRate(spinnerSelectedValue: String) {
+        _baseCurrencyRate.value = Transformations.map(rawAPIData){ currencyResponse ->
+            currencyResponse.quotes.toList().find { it.first.takeLast(3) == spinnerSelectedValue }
+        }.map { it?.second }.value
+//        val currency =rawAPIData.map { it.quotes.toList().map { it. } }
+//        val currencyAmount = currency?.convertedAmount
+        Log.i("transformedAPIData", transformedAPIData.value.toString())
+        Log.i("baseCurrencyRate", _baseCurrencyRate.value.toString())
     }
 
     /**
@@ -45,10 +61,7 @@ class CurrencyViewModel : ViewModel() {
     init {
         viewModelScope.launch {
             _rawAPIData.value = CurrencyApi.retrofitService.getCurrencies()
-//            val logTest= _rawAPIData.value
-            Log.i("rawApiData",rawAPIData.value.toString())
-//            val test = transformedAPIData.value
-//            Log.i("transformedAPIData", test.toString())
+            Log.i("rawApiData", rawAPIData.value.toString())
         }
 
     }
