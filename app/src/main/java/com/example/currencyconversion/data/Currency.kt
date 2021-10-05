de036ced6eeba48d34a373250adc316c214f6208
kareemadesola <kareemadesola1999@gmail.com>
2021-10-05 15:54:30 +0100

    define Currency data entity

--- app/src/main/java/com/example/currencyconversion/data/Currency.kt
+++ app/src/main/java/com/example/currencyconversion/data/Currency.kt
@@ -1,10 +1,16 @@
 package com.example.currencyconversion.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity data class represents a single row in a database
+ */
+@Entity
 data class Currency(
-    val success: Boolean,
-    val terms: String,
-    val privacy: String,
-    val timestamp: Int,
-    val source: String,
-    val quotes: Map<String,Double>
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "code") val currencyCode: String,
+    @ColumnInfo(name = "us_dollar_rate") val USDollarIndex: Double
 )
